7ee639a8ff4e615866a92139c707e50f5b16b61e
Nehuen Fortes <32969628+SoldierIced@users.noreply.github.com>
2024-07-03 15:19:49 -0300

    fix

--- routes/slack.route.js
+++ routes/slack.route.js
@@ -17,46 +17,48 @@ router.post('/challenge', async (req, res) => {
 
         let event = req.body.event;
         if (event.type == "message") {
-            if (event.channel == process.env.SLACK_CHANNEL) {
-                if (event.text.includes("list balances")) {
-                    await SlackController.listBalances();
-                }
-                if (event.text.includes("withdraw|")) {
-                    let args = event.text.split("|");
-                    console.log(args, "args")
-                    let amount = Number(args[2]);
-                    let balanceId = args[1];
-                    await SlackController.generateWithDraw(amount, balanceId);
-                    return res.send({});
-
-                }
-                if (event.text.includes("list withdraws")) {
-
-                    await SlackController.listWithDraws()
-
-
-                    return res.send({});
-
-                }
-                if (event.text.includes("withdraw status ")) {
-
-                    let args = event.text.split("|");
-                    console.log(args, "args")
-                    let status = args[1];
-                    switch (status) {
-                        case "pending": break;
-                        case "success": break;
-                        case "error": break;
-                        default:
-                            await SlackController.sendMessage("status should be: pending or success or error");
-                            return res.send({});
-                            break;
+            console.log(event)
+            if (event.text)
+                if (event.channel == process.env.SLACK_CHANNEL) {
+                    if (event.text.includes("list balances")) {
+                        await SlackController.listBalances();
                     }
-                    await SlackController.changeStatusWithdraw(args[0], status)
-                    return res.send({});
+                    if (event.text.includes("withdraw|")) {
+                        let args = event.text.split("|");
+                        console.log(args, "args")
+                        let amount = Number(args[2]);
+                        let balanceId = args[1];
+                        await SlackController.generateWithDraw(amount, balanceId);
+                        return res.send({});
 
+                    }
+                    if (event.text.includes("list withdraws")) {
+
+                        await SlackController.listWithDraws()
+
+
+                        return res.send({});
+
+                    }
+                    if (event.text.includes("withdraw status ")) {
+
+                        let args = event.text.split("|");
+                        console.log(args, "args")
+                        let status = args[1];
+                        switch (status) {
+                            case "pending": break;
+                            case "success": break;
+                            case "error": break;
+                            default:
+                                await SlackController.sendMessage("status should be: pending or success or error");
+                                return res.send({});
+                                break;
+                        }
+                        await SlackController.changeStatusWithdraw(args[0], status)
+                        return res.send({});
+
+                    }
                 }
-            }
         }
         res.send({ challenge: req.body.challenge }); return;
 
