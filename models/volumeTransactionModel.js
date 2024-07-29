795510baf051e4f44c62046c991daa949c12ad3e
Nehuen Fortes <32969628+SoldierIced@users.noreply.github.com>
2024-07-29 18:02:02 -0300

    fix import excel and new model

--- models/volumeTransactionModel.js
+++ models/volumeTransactionModel.js
@@ -1,41 +1,101 @@
 const mongoose = require('mongoose');
 
 const volumeTransactionSchema = new mongoose.Schema({
-    client: {
-        type: String,
-    },
     business: {
-        type: String,
+        type: String, // Corresponde a "Regulatory Period"
+        default: "Pre-Compliance"
+    },
+    client: {
+        type: String, // Corresponde a "Client Name"
     },
     jurisdiction: {
         type: String,
     },
     methodPay: {
         type: String,
+        default: "Wire"
     },
     date: {
         type: Date,
         default: Date.now,
-        required: true
+    },
+    receivedAmountEUR: {
+        type: String,
     },
     receivedAmount: {
-        type: Number,
-        required: true
+        type: String, // Corresponde a "receivedAmountUSD"
     },
-    symbol: {
+    totalReceived: {
         type: String,
-        required: true,
-        trim: true
+    },
+    shieldFee: {
+        type: String,
+    },
+    clientTransfer: {
+        type: String,
+    },
+    gasFees: {
+        type: String,
+    },
+    completed: {
+        type: Boolean,
+        default: false,
     },
     tx: {
         type: String,
         trim: true
     },
     walletSend: {
+        type: String, // Corresponde a "Wallet Address"
+        trim: true,
+    },
+    riskScore: {
+        type: String,
+    },
+    selfHosted: {
+        type: Boolean,
+        default: false,
+    },
+    kycAml: {
+        type: Boolean,
+        default: false,
+    },
+    partner: {
+        type: String,
+    },
+    onrampOfframp: {
+        type: String,
+    },
+    currencyPair: {
+        type: String,
+    },
+    blockchain: {
+        type: String,
+    },
+    notes: {
         type: String,
         trim: true
     },
-    excelLoad: { type: Boolean, default: false }
+    totalGmv: {
+        type: String,
+        trim: true
+    },
+    totalNetProfits: {
+        type: String,
+        trim: true
+    },
+    numberOfTransactions: {
+        type: String,
+        trim: true
+    },
+    excelLoad: {
+        type: Boolean,
+        default: false
+    },
+    symbol: {
+        type: String,
+        default: "USDT" // Default value
+    }
 });
 
 module.exports = mongoose.model('VolumeTransaction', volumeTransactionSchema);
