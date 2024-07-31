d69e20ce11034433e3e0f208dcfb153ebf848a58
Nehuen Fortes <32969628+SoldierIced@users.noreply.github.com>
2024-07-31 13:49:40 -0300

    footprint services

--- routes/bank.route.js
+++ routes/bank.route.js
@@ -19,40 +19,38 @@ const handleResponse = (respon, res) => {
 };
 // Validaciones para el cuerpo de la solicitud
 const bankAccountValidation = [
-    body('account_holder_name').notEmpty().withMessage('Account holder name is required'),
-    body('account_number').notEmpty().withMessage('Account number is required'),
-    body('routing_number').notEmpty().withMessage('Routing number is required'),
-    body('bank_name').notEmpty().withMessage('Bank name is required'),
-    body('account_type').notEmpty().withMessage('Account type is required'),
-    body('address.street').notEmpty().withMessage('Street is required'),
-    body('address.city').notEmpty().withMessage('City is required'),
-    body('address.state').notEmpty().withMessage('State is required'),
-    body('address.zip_code').notEmpty().withMessage('Zip code is required'),
-    // body('contact_details.email').isEmail().withMessage('Valid email is required'),
-    body('contact_details.phone').notEmpty().withMessage('Phone number is required')
+    body('custom.beneficiary_name').notEmpty().withMessage('Beneficiary name is required'),
+    body('custom.bank_name').notEmpty().withMessage('Bank name is required'),
+    body('custom.account_number').notEmpty().withMessage('Account number is required'),
+    body('custom.routing_number').notEmpty().withMessage('Routing number is required'),
+    body('custom.country').notEmpty().withMessage('Country is required'),
+    body('custom.street_address').notEmpty().withMessage('Street address is required'),
+    body('custom.city').notEmpty().withMessage('City is required'),
+    body('custom.state').notEmpty().withMessage('State is required'),
+    body('custom.zip_code').notEmpty().withMessage('Zip code is required')
 ];
 // Función para manejar errores de validación y devolverlos como un solo string
 const formatValidationErrors = (errors) => {
     return errors.map(err => `${err.param}: ${err.msg}`).join(', ');
 };
 // Crear una nueva cuenta bancaria (POST)
-router.post('/', auth, bankAccountValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(200).json(response(formatValidationErrors(errors.array()), "error"));
-        }
+// router.post('/', auth, bankAccountValidation, async (req, res) => {
+//     try {
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(200).json(response(formatValidationErrors(errors.array()), "error"));
+//         }
 
-        const resp = await fetch(externalServerUrl + "bank/" + req.user._id, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(req.body)
-        });
-        return handleResponse(resp, res);
-    } catch (error) {
-        res.status(200).json(response('Error creating bank account', "error"));
-    }
-});
+//         const resp = await fetch(externalServerUrl + "bank/" + req.user._id, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify(req.body)
+//         });
+//         return handleResponse(resp, res);
+//     } catch (error) {
+//         res.status(200).json(response('Error creating bank account', "error"));
+//     }
+// });
 // actualizar una cuenta bancaria (POST)
 router.put('/', auth, bankAccountValidation, async (req, res) => {
     try {
@@ -60,14 +58,19 @@ router.put('/', auth, bankAccountValidation, async (req, res) => {
         if (!errors.isEmpty()) {
             return res.status(200).json(response(formatValidationErrors(errors.array()), "error"));
         }
-
-        const resp = await fetch(externalServerUrl + "bank/" + req.user._id, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
+        if (!req.user.footId) {
+            return res.status(200).json(response("User foot Id not found", "error"));
+        }
+        req.user.footId = "fp_id_test_Ma33pMQFO4MCgJ8WVwMRfY"
+        const resp = await fetch("https://api.onefootprint.com/users/" + req.user.footId + "/vault", {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json', "X-Footprint-Secret-Key": process.env.FOOTPRINT_SECRET_KEY },
             body: JSON.stringify(req.body)
         });
+        console.log(resp)
         return handleResponse(resp, res);
     } catch (error) {
+        console.log(error)
         res.status(200).json(response('Error updating bank account', "error"));
     }
 });
@@ -90,11 +93,28 @@ router.get('/verify', auth, async (req, res) => {
 // Obtener la cuenta bancaria del usuario
 router.get('/', auth, async (req, res) => {
     try {
-        const resp = await fetch(externalServerUrl + "bank/" + req.user._id, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
+        req.user.footId = "fp_id_test_Ma33pMQFO4MCgJ8WVwMRfY"
+        const resp = await fetch("https://api.onefootprint.com/users/" + req.user.footId + "/vault/decrypt", {
+            method: 'POST',
+            headers: { "X-Footprint-Secret-Key": process.env.FOOTPRINT_SECRET_KEY },
+            body: JSON.stringify({
+                "fields": [
+                    "custom.beneficiary_name",
+                    "custom.country",
+                    "custom.city",
+                    "custom.routing_number",
+                    "custom.zip_code",
+                    "custom.account_number",
+                    "custom.state",
+                    "custom.bank_name",
+                    "custom.street_address"
+                ],
+                "reason": "Getting client bank details"
+            })
         });
-        return handleResponse(resp, res);
+
+        return res.status(200).json(response(await resp.json()))
+        return
     } catch (error) {
         res.status(200).json(response('Error fetching bank accounts', "error"));
     }
