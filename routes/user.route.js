34cd38b05511473e6f0f4f8c8a3b81b520f48a77
LucasACH <achaval.lucas@gmail.com>
2024-08-12 19:32:51 -0300

    chore: Add use_implicit_auth: true attribute to the request body

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,18 +1,18 @@
-const auth = require('../middleware/auth');
-const logRequest = require('../middleware/logRequest');
+const auth = require("../middleware/auth");
+const logRequest = require("../middleware/logRequest");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controllers/user.controller');
-const userModel = require('../models/user.model');
-const { response, footPrintUser } = require('../utils');
+const userController = require("../controllers/user.controller");
+const userModel = require("../models/user.model");
+const { response, footPrintUser } = require("../utils");
 
-router.get('/', auth, logRequest, userController.allUsers);
-router.get('/:id', auth, logRequest, userController.getUserById);
-router.post('/', auth, logRequest, userController.createUser);
-router.put('/:id', auth, logRequest, userController.updateUserById);
-router.post('/user/validate', auth, async (req, res) => {
+router.get("/", auth, logRequest, userController.allUsers);
+router.get("/:id", auth, logRequest, userController.getUserById);
+router.post("/", auth, logRequest, userController.createUser);
+router.put("/:id", auth, logRequest, userController.updateUserById);
+router.post("/user/validate", auth, async (req, res) => {
   const data = await footPrintUser(req.body.validation_token);
   if (data?.user_auth?.fp_id) {
     req.user.footId = data?.user_auth?.fp_id;
@@ -22,12 +22,12 @@ router.post('/user/validate', auth, async (req, res) => {
   return res.send(response(req.user));
 });
 
-router.get('/user/foot/:id', auth, async (req, res) => {
+router.get("/user/foot/:id", auth, async (req, res) => {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-      'X-Footprint-Secret-Key': process.env.FOOTPRINT_SECRET_KEY,
+      "Content-Type": "application/json",
+      "X-Footprint-Secret-Key": process.env.FOOTPRINT_SECRET_KEY,
     },
   };
   const resp = await fetch(
@@ -39,16 +39,17 @@ router.get('/user/foot/:id', auth, async (req, res) => {
   return res.send(response(data));
 });
 
-router.post('/foot/token/:foot_pb', auth, async (req, res) => {
+router.post("/foot/token/:foot_pb", auth, async (req, res) => {
   const options = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'X-Footprint-Secret-Key': process.env.FOOTPRINT_SECRET_KEY,
+      "Content-Type": "application/json",
+      "X-Footprint-Secret-Key": process.env.FOOTPRINT_SECRET_KEY,
     },
     body: JSON.stringify({
       key: req.params.foot_pb,
-      kind: 'onboard',
+      kind: "onboard",
+      use_implicit_auth: true,
     }),
   };
 
