7b78bc614adabf7f317f57978133eebcc83534d5
Fabricio Richieri <frichieri.dev@gmail.com>
2024-08-26 15:36:37 -0300

    email templates UI adjustment

--- routes/quotes.route.js
+++ routes/quotes.route.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const z = require("zod");
+const express = require('express');
+const z = require('zod');
 const {
   getRampQuote,
   createPayPalOrder,
-} = require("../controllers/quote.controller");
-const auth = require("../middleware/auth");
-const { sendGroupMessage } = require("../utils");
+} = require('../controllers/quote.controller');
+const auth = require('../middleware/auth');
+const { sendGroupMessage } = require('../utils');
 const {
   sendPayPalOrderApprovedEmail,
-} = require("../controllers/email.controller");
-const { log } = require("handlebars/runtime");
+} = require('../controllers/email.controller');
+const { log } = require('handlebars/runtime');
 
 const router = express.Router();
 
 const QuoteParamsSchema = z.object({
-  type: z.enum(["onramp", "offramp"]),
+  type: z.enum(['onramp', 'offramp']),
 });
 
 const QuoteQuerySchema = z.object({
@@ -23,7 +23,7 @@ const QuoteQuerySchema = z.object({
   amountIn: z.string(),
 });
 
-router.get("/:type", async function (req, res, next) {
+router.get('/:type', async function (req, res, next) {
   try {
     const { type } = QuoteParamsSchema.parse(req.params);
     const { assetIn, assetOut, amountIn } = QuoteQuerySchema.parse(req.query);
@@ -33,7 +33,7 @@ router.get("/:type", async function (req, res, next) {
       assetIn.toUpperCase(),
       assetOut.toUpperCase(),
       amountIn,
-      "0.01"
+      '0.01'
     );
 
     if (quote.error) {
@@ -46,10 +46,10 @@ router.get("/:type", async function (req, res, next) {
   }
 });
 
-router.post("/onramp/paypal", auth, async function (req, res, next) {
+router.post('/onramp/paypal', auth, async function (req, res, next) {
   try {
     const { encoded, network, wallet } = req.body;
-    console.log("/onramp/paypal", req.body);
+    console.log('/onramp/paypal', req.body);
     const { id } = await createPayPalOrder(encoded, network, wallet);
     return res.send({ id });
   } catch (err) {
@@ -57,14 +57,14 @@ router.post("/onramp/paypal", auth, async function (req, res, next) {
   }
 });
 
-router.post("/onramp/paypal/webhook", async function (req, res, next) {
+router.post('/onramp/paypal/webhook', async function (req, res, next) {
   try {
     const body = req.body;
 
     const email = body.resource.payment_source.paypal.email_address;
 
     const [cryptoAmount, cryptoSymbol, network, wallet] =
-      body.resource.purchase_units[0].description.split(" ");
+      body.resource.purchase_units[0].description.split(' ');
 
     const order = {
       name: body.resource.payment_source.paypal.name.given_name,
@@ -103,7 +103,17 @@ Payment Source: PayPal
     await sendGroupMessage(message);
     await sendPayPalOrderApprovedEmail(email, order);
 
-    return res.send("OK");
+    return res.send('OK');
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('/onramp/payoneer', async function (req, res, next) {
+  try {
+    const { encoded, asset, wallet } = req.body;
+    // const { id } = await createPayoneerOrder(encoded, asset, wallet);
+    // return res.send({ id });
   } catch (err) {
     return next(err);
   }
